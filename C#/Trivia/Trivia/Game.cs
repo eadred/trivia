cac799c0ec1df98cf6333bcb16430965ff2701cc
Eadred <eadred.birchenough@zuhlke.com>
2016-07-23 16:40:06 +0100

    Using lookup in currentCategory()

--- C#/Trivia/Trivia/Game.cs
+++ C#/Trivia/Trivia/Game.cs
@@ -123,16 +123,15 @@ namespace UglyTrivia
 
         private String currentCategory()
         {
-            if (places[currentPlayer] == 0) return "Pop";
-            if (places[currentPlayer] == 4) return "Pop";
-            if (places[currentPlayer] == 8) return "Pop";
-            if (places[currentPlayer] == 1) return "Science";
-            if (places[currentPlayer] == 5) return "Science";
-            if (places[currentPlayer] == 9) return "Science";
-            if (places[currentPlayer] == 2) return "Sports";
-            if (places[currentPlayer] == 6) return "Sports";
-            if (places[currentPlayer] == 10) return "Sports";
-            return "Rock";
+            var cats = new Dictionary<int, string>
+            {
+                {0, "Pop"},
+                {1, "Science"},
+                {2, "Sports"},
+                {3, "Rock"}
+            };
+
+            return cats[places[currentPlayer]%4];
         }
 
         public bool wasCorrectlyAnswered()
